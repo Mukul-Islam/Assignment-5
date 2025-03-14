5e0813df9f6f63207f689d064f55db78e3ade3b2
Mukul-Islam <137820867+Mukul-Islam@users.noreply.github.com>
2025-03-14 06:33:55 +0600

    added a backbutton to this field

--- js/button_disabled.js
+++ js/button_disabled.js
@@ -2,3 +2,7 @@ document.getElementById('discovery-page')
 .addEventListener('click',function(){
     window.location.href = './discovery_page.html';
 })
+document.getElementById('back-btn')
+.addEventListener('click',function(){
+    window.location.href = './index.html';
+})
